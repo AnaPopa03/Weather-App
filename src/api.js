096e60223f68e45c89ea1fdfff980638ecefb880
AnaPopa <ana.popa032821@yahoo.com>
2023-04-12 09:29:15 +0300

    final project

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '12f22cb102mshdb07c8746aace99p13faa4jsn14716cf231c3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "108bfdb7451aaf091a9fd0b25cfa12b9";
